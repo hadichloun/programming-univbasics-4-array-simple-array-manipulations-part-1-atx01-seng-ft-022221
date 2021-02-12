96bfe4e021241c5fbab0012bcdaefc414cc27b19
Hadi Chloun <hadi_960@hotmail.com>
2021-02-12 02:59:06 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,28 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  element=array.pop
+  element
+end
+
+def pop_with_args(array)
+  elements=array.pop(2)
+  elements
+end
+
+def using_shift(array)
+  element=array.shift
+  element
+end
+
+def shift_with_args(array)
+  elements=array.shift(2)
+  elements
+end
+  
